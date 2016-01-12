61c6f1f2459387addb11b1976f4bebd5d8985083
kamrenz <kamren@developintelligence.com>
2016-01-12 15:18:29 -0700

    Updating to Angular 2 Beta

---
+++ src/bootstrap.js
@@ -0,0 +1,4 @@
+var browser_1 = require("angular2/platform/browser");
+var hello_js_1 = require("./components/hello.js");
+//Bootstrapping main Angular Component
+browser_1.bootstrap(hello_js_1.HelloComponent);
